dd57566f2889b2ab66d47fb4552ba2ab9104417b
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-10 23:56:56 +0200

    Create brand db entity

---
+++ TechZoneAPI/Data/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace TechZoneAPI.Data
+{
+    public class DataConstants
+    {
+        public class Common
+        {
+            public const int IdMaxLength = 40;
+        }
+
+        public class Brand
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+    }
+}
