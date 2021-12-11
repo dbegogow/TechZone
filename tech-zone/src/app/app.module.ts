29e431775705a154a36e2965fb512555514773b0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-11 18:05:03 +0200

    Implement categories component

--- tech-zone/src/app/app.module.ts
+++ tech-zone/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
