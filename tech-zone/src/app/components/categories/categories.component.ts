c04dfaadd7f1d4192bda75d1c21efdfd4db5aa5e
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-12 00:16:45 +0200

    Implement categories component

--- tech-zone/src/app/components/categories/categories.component.ts
+++ tech-zone/src/app/components/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  @Input() openedCategories: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openCategories(categoriesType: string) {
+    this.openedCategories = categoriesType;
+  }
+
+  closeCategories() {
+    this.openedCategories = '';
+  }
 }
