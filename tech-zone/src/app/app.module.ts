86770d792b567d9c22c6dc6ec4e4e6c2eead458a
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-13 16:57:13 +0200

    Add slider component

--- tech-zone/src/app/app.module.ts
+++ tech-zone/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
