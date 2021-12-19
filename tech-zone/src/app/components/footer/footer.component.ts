873e15578dd3fe73ed7fef0cc47272fde32fd82b
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-20 00:20:26 +0200

    Implement links container in footer component

--- tech-zone/src/app/components/footer/footer.component.ts
+++ tech-zone/src/app/components/footer/footer.component.ts
@@ -6,6 +6,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  linksData = [
+    {
+      title: 'TechZone',
+      links: [
+        {
+          name: 'About Us',
+          path: '/'
+        },
+        {
+          name: 'Careers',
+          path: '/'
+        },
+        {
+          name: 'News',
+          path: '/'
+        },
+        {
+          name: 'Terms and Conditions',
+          path: '/'
+        }
+      ]
+    },
+    {
+      title: 'Contacts',
+      links: [
+        {
+          name: 'Contacts',
+          path: '/'
+        },
+        {
+          name: 'Stores',
+          path: '/'
+        },
+        {
+          name: 'Repair shops',
+          path: '/'
+        },
+        {
+          name: `Holiday's working time`,
+          path: '/'
+        }
+      ]
+    },
+    {
+      title: 'Clients',
+      links: [
+        {
+          name: 'Advices',
+          path: '/'
+        },
+        {
+          name: 'Transport',
+          path: '/'
+        },
+        {
+          name: 'Delivery for online purchases',
+          path: '/'
+        },
+        {
+          name: 'Doorstep delivery when purchased from physical stores',
+          path: '/'
+        },
+        {
+          name: 'Pick from store',
+          path: '/'
+        },
+        {
+          name: 'Services',
+          path: '/'
+        },
+        {
+          name: 'Order return',
+          path: '/'
+        }
+      ]
+    },
+    {
+      title: 'Information',
+      links: [
+        {
+          name: 'How to order',
+          path: '/'
+        },
+        {
+          name: 'Sanitary and hygienic conditions',
+          path: '/'
+        },
+        {
+          name: 'Recycling',
+          path: '/'
+        }
+      ]
+    }
+  ];
 
   constructor() { }
 
