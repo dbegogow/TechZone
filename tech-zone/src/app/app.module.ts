e07af574932bd0555c73035ae88164dc04351cb0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-01-08 22:41:03 +0200

    Implement cart component with product component

--- tech-zone/src/app/app.module.ts
+++ tech-zone/src/app/app.module.ts
@@ -42,6 +42,8 @@ import { NoProductsComponent } from './components/account/orders-processing/no-p
 import { OrdersProcessingProductComponent } from './components/account/orders-processing/orders-processing-product/orders-processing-product.component';
 import { FavouriteProductsComponent } from './components/favourite-products/favourite-products.component';
 import { FavouriteProductComponent } from './components/favourite-products/favourite-product/favourite-product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CartProductComponent } from './components/cart/cart-product/cart-product.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -56,6 +58,7 @@ const appRoutes: Routes = [
   { path: 'working-time', component: WorkingTimeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'favourites', component: FavouriteProductsComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {
@@ -108,7 +111,9 @@ const appRoutes: Routes = [
     NoProductsComponent,
     OrdersProcessingProductComponent,
     FavouriteProductsComponent,
-    FavouriteProductComponent
+    FavouriteProductComponent,
+    CartComponent,
+    CartProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ tech-zone/src/app/components/cart/cart-product/cart-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-product',
+  templateUrl: './cart-product.component.html',
+  styleUrls: ['./cart-product.component.css']
+})
+export class CartProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
