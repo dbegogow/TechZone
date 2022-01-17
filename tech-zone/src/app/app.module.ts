411db339333b08334a74d08fa26742e2cc7a9ab2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-01-17 17:43:17 +0200

    Implement getting questions from server

--- tech-zone/src/app/app.module.ts
+++ tech-zone/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -128,6 +129,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'enabled' })
   ],
   providers: [],
--- tech-zone/src/app/services/questions/questions.service.ts
+++ tech-zone/src/app/services/questions/questions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from '../../../environments/environment.dev';
 import { httpOptions } from '../http-options';
 import { IQuestion } from '../../interfaces/questions/question';
@@ -9,10 +9,13 @@ import { IQuestion } from '../../interfaces/questions/question';
   providedIn: 'root'
 })
 export class QuestionsService {
+  private apiUrl: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.apiUrl = `${environment.apiUrl}api/questions`;
+  }
 
   getQuestions(): Observable<IQuestion[]> {
-    return this.http.get<IQuestion[]>(environment.apiUrl);
+    return this.http.get<IQuestion[]>(this.apiUrl);
   }
 }
