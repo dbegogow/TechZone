e4d35a2d4a1ffb042cdfb45b448eb881b4b6bdc3
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-01-22 19:04:24 +0200

    Add id to user entity

--- TechZoneAPI/Data/Models/User.cs
+++ TechZoneAPI/Data/Models/User.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 using static TechZoneAPI.Data.DataConstants.User;
+using static TechZoneAPI.Data.DataConstants.Common;
 
 namespace TechZoneAPI.Data.Models
 {
-    public class User : IdentityUser
+    public class User
     {
+        [Key]
+        [Required]
+        [MaxLength(IdMaxLength)]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
         [MaxLength(NameMaxLength)]
         public string FirstName { get; set; }
 
